2b1e41b9382e2f2dd6a6769106ce8a666bc5a508
Raghav Agarwal <agarwalraghav0703@gmail.com>
2021-12-10 09:25:48 +0530

    Added reset logic

---
+++ dist/main.1f19ae8e.js
@@ -0,0 +1,449 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"main.js":[function(require,module,exports) {
+'use strict';
+
+var direction = 'right';
+var element = document.querySelector("#" + "snake");
+var style = getComputedStyle(element);
+var score = 0;
+var speed = 20;
+document.getElementById("labelScore").innerText = score;
+setFoodPosition();
+setObstaclePosition();
+setScorePosition();
+setInterval(function () {
+  var snakeCollideWithFood = isCollide(document.getElementById('snake'), document.getElementById('food'));
+  var snakeCollideWithobstacle = isCollide(document.getElementById('snake'), document.getElementById('obstacle'));
+  var snakeCollideWithBox = isCollide(document.getElementById('snake'), document.getElementById('box'));
+
+  if (snakeCollideWithFood) {
+    handleSnakeFoodCollision();
+    setFoodPosition();
+  }
+
+  if (snakeCollideWithobstacle || !snakeCollideWithBox) {
+    alert('Game over'); // setObstaclePosition();
+
+    resetGame();
+  }
+
+  document.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+      case 37:
+        direction = 'left';
+        break;
+
+      case 38:
+        direction = 'up';
+        break;
+
+      case 39:
+        direction = 'right';
+        break;
+
+      case 40:
+        direction = 'down';
+        break;
+    }
+  });
+
+  if (direction == 'left') {
+    moveSnakeLeft();
+  } else if (direction == 'up') {
+    moveSnakeUp();
+  } else if (direction == 'down') {
+    moveSnakeDown();
+  } else {
+    moveSnakeRight();
+  }
+}, 1000);
+
+function moveSnakeRight() {
+  var newPos = parseInt(style.marginLeft) + speed + "px";
+  document.getElementById("snake").style.marginLeft = newPos;
+}
+
+function moveSnakeLeft() {
+  var newPos = parseInt(style.marginLeft) - speed + "px";
+  document.getElementById("snake").style.marginLeft = newPos;
+}
+
+function moveSnakeDown() {
+  var newPos = parseInt(style.marginTop) + speed + "px";
+  document.getElementById("snake").style.marginTop = newPos;
+}
+
+function moveSnakeUp() {
+  var newPos = parseInt(style.marginTop) - speed + "px";
+  document.getElementById("snake").style.marginTop = newPos;
+}
+
+function isCollide(a, b) {
+  var aRect = a.getBoundingClientRect();
+  var bRect = b.getBoundingClientRect();
+  return !(aRect.top + aRect.height < bRect.top || aRect.top > bRect.top + bRect.height || aRect.left + aRect.width < bRect.left || aRect.left > bRect.left + bRect.width);
+}
+
+function setFoodPosition() {
+  var topRandom = (Math.random() * (5 - 1.6) + 1.6) * 100;
+  var leftRandom = (Math.random() * (7.5 - 2) + 2) * 100;
+  document.getElementById("food").style.top = Math.floor(topRandom) + "px";
+  document.getElementById("food").style.left = Math.floor(leftRandom) + "px";
+}
+
+function setObstaclePosition() {
+  var topRandom = (Math.random() * (5 - 1.6) + 1.6) * 100;
+  var leftRandom = (Math.random() * (7.5 - 2) + 2) * 100;
+  document.getElementById("obstacle").style.top = Math.floor(topRandom) + "px";
+  document.getElementById("obstacle").style.left = Math.floor(leftRandom) + "px";
+}
+
+function setScorePosition() {
+  document.getElementById("score").style.top = "550px";
+  document.getElementById("score").style.left = "468px";
+}
+
+function handleSnakeFoodCollision() {
+  score = score + 1;
+  speed = speed + 2;
+  var newWidthOfSnake = parseInt(style.width) + 5 + "px";
+  document.getElementById("snake").style.width = newWidthOfSnake;
+  console.log(score);
+  document.getElementById("labelScore").innerText = score;
+}
+
+function resetGame() {
+  score = 0;
+  direction = 'right';
+  document.getElementById("labelScore").innerText = score;
+  document.getElementById("snake").style.width = "50px";
+  document.getElementById("snake").style.height = "10px";
+  document.getElementById("snake").style.marginTop = "100px";
+  document.getElementById("snake").style.marginLeft = "150px";
+  setFoodPosition();
+  setObstaclePosition();
+}
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58620" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.1f19ae8e.js.map
--- src/main.js
+++ src/main.js
@@ -2,14 +2,27 @@
 var direction = 'right';
 const element = document.querySelector("#"+"snake");
 const style = getComputedStyle(element);
+var score = 0;
+var speed = 20;
+document.getElementById("labelScore").innerText = score;
+setFoodPosition();
+setObstaclePosition();
+setScorePosition();
 setInterval(function(){
+  
   var snakeCollideWithFood = isCollide(document.getElementById('snake'),document.getElementById('food'));
-  if(snakeCollideWithFood)
+  var snakeCollideWithobstacle = isCollide(document.getElementById('snake'),document.getElementById('obstacle'));
+  var snakeCollideWithBox = isCollide(document.getElementById('snake'),document.getElementById('box'));
+  if(snakeCollideWithFood )
   {
-    console.log("h1");
-    document.getElementById('food').marginLeft = Math.floor(Math.random() * 100) + "px";
-    console.log(document.getElementById('food').marginLeft);
-   // document.getElementById('food').marginLeft = Math.floor(Math.random() * 100) + "px";
+    handleSnakeFoodCollision();
+    setFoodPosition();
+  }
+  if(snakeCollideWithobstacle|| !snakeCollideWithBox)
+  {
+    alert('Game over');
+   // setObstaclePosition();
+    resetGame();
   }
   document.addEventListener('keydown', function(e) {
     switch (e.keyCode) {
@@ -21,7 +34,6 @@ setInterval(function(){
             break;
         case 39:
           direction = 'right'
-          moveSnakeRight()
             break;
         case 40:
           direction = 'down'
@@ -40,28 +52,27 @@ setInterval(function(){
      {
        moveSnakeDown();
      }
-     else
+     else 
      {
        moveSnakeRight();
      }
   }, 1000);
 function moveSnakeRight() {
-  const newPos = parseInt(style.marginLeft)+ 20  + "px";
+  const newPos = parseInt(style.marginLeft)+ speed  + "px";
   document.getElementById("snake").style.marginLeft = newPos;
 }
 function moveSnakeLeft() {
-  const newPos = parseInt(style.marginLeft)- 20  + "px";
+  const newPos = parseInt(style.marginLeft)- speed  + "px";
   document.getElementById("snake").style.marginLeft = newPos;
 }
 function moveSnakeDown()
 {
-  const newPos = parseInt(style.marginTop)+ 20  + "px";
+  const newPos = parseInt(style.marginTop)+ speed  + "px";
   document.getElementById("snake").style.marginTop = newPos;
-
 }
 function moveSnakeUp()
 {
-  const newPos = parseInt(style.marginTop)- 20  + "px";
+  const newPos = parseInt(style.marginTop)- speed  + "px";
   document.getElementById("snake").style.marginTop = newPos;
 }
 function isCollide(a, b) {
@@ -74,4 +85,48 @@ function isCollide(a, b) {
       ((aRect.left + aRect.width) < bRect.left) ||
       (aRect.left > (bRect.left + bRect.width))
   );
+}
+function setFoodPosition()
+{
+
+  var topRandom = (Math.random() * (5 - 1.6) + 1.6)*100;
+  var leftRandom = (Math.random() * (7.5 - 2) + 2)*100;
+  document.getElementById("food").style.top = Math.floor(topRandom) + "px";
+  document.getElementById("food").style.left = Math.floor(leftRandom) + "px";
+}
+function setObstaclePosition()
+{
+  var topRandom = (Math.random() * (5 - 1.6) + 1.6)*100;
+  var leftRandom = (Math.random() * (7.5 - 2) + 2)*100;
+  document.getElementById("obstacle").style.top = Math.floor(topRandom) + "px";
+  document.getElementById("obstacle").style.left = Math.floor(leftRandom) + "px";
+}
+function setScorePosition()
+{
+  document.getElementById("score").style.top =   "550px";
+  document.getElementById("score").style.left = "468px";
+}
+
+function handleSnakeFoodCollision()
+{
+  score = score +1;
+  speed = speed +2;
+  const newWidthOfSnake = parseInt(style.width)+ 5 +"px";
+  document.getElementById("snake").style.width = newWidthOfSnake;
+  console.log(score);
+  document.getElementById("labelScore").innerText = score;
+}
+function resetGame()
+{
+  score = 0;
+  direction = 'right';
+  document.getElementById("labelScore").innerText = score;
+  document.getElementById("snake").style.width = "50px";
+  document.getElementById("snake").style.height = "10px";
+  document.getElementById("snake").style.marginTop = "100px";
+  document.getElementById("snake").style.marginLeft = "150px";
+  setFoodPosition();
+  setObstaclePosition();
+
+
 }
